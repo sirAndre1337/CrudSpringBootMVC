c6e7c6f0fe69ea51800db0c54c01e30eec40f4da
Andre Luis <andrelluis17@hotmail.com>
2021-09-21 15:49:19 -0300

    Consumindo web service restfull cep

--- src/main/java/com/example/curso/model/Pessoa.java
+++ src/main/java/com/example/curso/model/Pessoa.java
@@ -39,6 +39,49 @@ public class Pessoa implements Serializable{
 	@OneToMany(mappedBy = "pessoa" , fetch = FetchType.EAGER , cascade = CascadeType.REMOVE)
 	private List<Telefone> telefones;
 	
+	private String cep;
+	private String rua;
+	private String bairro;
+	private String cidade;
+	private String uf;
+	private String ibge;
+	
+	public String getUf() {
+		return uf;
+	}
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getIbge() {
+		return ibge;
+	}
+	public void setIbge(String ibge) {
+		this.ibge = ibge;
+	}
 	public List<Telefone> getTelefones() {
 		return telefones;
 	}
