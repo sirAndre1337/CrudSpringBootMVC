6bb476edb4cc9366c927085ae2174670f51ae739
Andre Luis <andrelluis17@hotmail.com>
2021-09-22 16:25:44 -0300

    Mudando o ENUM para ter a descricao

--- src/main/java/com/example/curso/model/Cargo.java
+++ src/main/java/com/example/curso/model/Cargo.java
@@ -2,8 +2,21 @@ package com.example.curso.model;
 
 public enum Cargo {
 
-	JUNIOR,
-	PLENO,
-	SENIOR
+	JUNIOR ("Júnior (Comeco de carreira)"),
+	PLENO ("Pleno (Alguns anos de XP)"),
+	SENIOR("Senio (Pika das Galaxias)");
 	
+	private String nome;
+	
+	private Cargo(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 }
